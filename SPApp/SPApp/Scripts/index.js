0fe4067ad8d63350d921b5d4602a3aaa235b593e
mb5975 <mb5975@student.uni-lj.si>
2016-11-06 15:20:39 +0100

    responsive design added for home and item details and tables

---
+++ SPApp/SPApp/Scripts/index.js
@@ -0,0 +1,32 @@
+﻿//register fading of background image
+document.addEventListener('scroll', fader);
+
+//register scroll event 
+window.addEventListener("scroll", toggleFixedMenu);
+
+
+
+
+
+function fader() {
+    var scrolledFromTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    console.log(scrolledFromTop);
+    if (scrolledFromTop > 200) {
+        document.getElementsByClassName("divWithBackgroundImage")[0].style.opacity = Math.min(1, Math.max(1 - scrolledFromTop / 500 + 0.4, 0));
+    }
+    else {
+        document.getElementsByClassName("divWithBackgroundImage")[0].style.opacity = 1;
+    }
+
+}
+
+function toggleFixedMenu() {
+    var fromTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
+    if (fromTop > 800) {
+        document.getElementsByClassName("fixed-index-header")[0].style.visibility = 'visible';
+    }
+    else if (fromTop < 800) {
+        var m = document.getElementsByClassName("fixed-index-header")[0].style.visibility = 'hidden';
+    }
+
+}
--- SPApp/SPApp/Scripts/site.js
+++ SPApp/SPApp/Scripts/site.js
@@ -1,36 +1,4 @@
-﻿//register fading of background image
-document.addEventListener('scroll', fader);
-
-    //register scroll event 
-window.addEventListener("scroll", toggleFixedMenu);
-window.addEventListener('scroll', animate_elems);
-
-
-
-
-function fader() {
-    var scrolledFromTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-    console.log(scrolledFromTop);
-    if (scrolledFromTop > 200) {
-        document.getElementsByClassName("divWithBackgroundImage")[0].style.opacity = Math.min(1, Math.max(1 - scrolledFromTop / 500 + 0.4, 0));
-    }
-    else {
-        document.getElementsByClassName("divWithBackgroundImage")[0].style.opacity = 1;
-    }
-
-}
-
-function toggleFixedMenu() {
-    var fromTop = (window.pageYOffset !== undefined) ? window.pageYOffset : (document.documentElement || document.body.parentNode || document.body).scrollTop;
-    if (fromTop > 800) {
-        document.getElementsByClassName("fixed-index-header")[0].style.visibility = 'visible';
-    }
-    else if (fromTop < 800) {
-        var m = document.getElementsByClassName("fixed-index-header")[0].style.visibility = 'hidden';
-    }
-
-}
-
+﻿window.addEventListener('scroll', animate_elems);
 
 function animate_elems() {
     var elements = document.getElementsByClassName('animateblock');
