c1da372f9a7466ddf4bb59599609a231ae7030c1
mb5975 <mb5975@student.uni-lj.si>
2016-11-20 15:27:33 +0100

    nav popravljen, registration js v svoj file, enako search.js in itemdetails style v css

---
+++ SPApp/SPApp/Scripts/registration.js
@@ -0,0 +1,17 @@
+﻿function validatePassword() {
+    var password = document.getElementById("password");
+    var confirm_password = document.getElementById("confirm-password");
+
+    if (confirm_password.value == "") {
+        confirm_password.setCustomValidity("Polje ne sme biti prazno.");
+    }
+
+    if (password.value != confirm_password.value) {
+        confirm_password.setCustomValidity("Gesli se ne ujemata.");
+        return false;
+    } else {
+        confirm_password.setCustomValidity('');
+        return true;
+    }
+
+}
---
+++ SPApp/SPApp/Scripts/search.js
@@ -0,0 +1,32 @@
+﻿function showResults() {
+    document.getElementById('result-container').style.visibility = 'visible';
+}
+
+//za post je treba sam prvega dat praznega (value) in required na select in bo delal
+function validate_search() {
+    var selectedIndex = document.querySelector('#category-and-search-input-container select').selectedIndex;
+    var selectedValue = document.querySelector('#category-and-search-input-container select').value;
+
+    //var searchFilter = document.getElementById('search-filter').value;
+    //if (selectedIndex == 0 || searchFilter == "") {
+    if (selectedValue == 'disabled') { //dela tudi tam, kjer ni prvega disabled za razliko od indexa
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+function submit_search() {
+    var validation_successful = validate_search();
+    if (validation_successful) {
+        //submit
+        console.log("validation successful");
+        showResults();
+        return true;
+    }
+    else {
+        //set custom valididty
+        return false;
+    }
+}
